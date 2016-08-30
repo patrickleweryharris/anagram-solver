3da5a0c57cb0d57f3513ce372e1557953a752172
Patrick Harris <patricklewery.harris@mail.utoronto.ca>
2016-08-30 14:47:53 -0400

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,46 @@
+# anagram-solver
+
+![banner](anagram.se/wp-content/themes/anagramproduktion.se/img/logo.png)
+
+![python](https://img.shields.io/badge/python-3.5-blue.svg?style=flat-square) [![License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)](https://github.com/patrickleweryharris/anagram-solver/blob/master/LICENSE) [![standard-readme compliant](https://img.shields.io/badge/standard--readme-OK-green.svg?style=flat-square)](https://github.com/RichardLitt/standard-readme)
+
+> Solve anagrams with python
+
+This program provides solutions to anagrams. Solutions come from a list of words that includes 99172 common english words.
+
+## Table of Contents
+
+- [Installation](#install)
+- [Usage](#usage)
+- [Contribute](#contribute)
+- [License](#license)
+
+## Install
+
+```shell
+$ cd ~/place-where-you-put-things # Whatever directory you want
+$ git clone # TODO clone url here
+$ cd anagram-solver/solvers
+$ chmod +x anagram_solver.py # Permission must be granted for the script to run
+```
+
+## Usage
+The solver used must be in the same directory as the **words** file
+
+```shell
+$ cd ~/place-where-you-put-things/anagram-solver/solvers # Path to the solvers file
+$ ./anagram_solver.py
+Type an anagram: # Your anagram here!
+Solutions:
+# Solutions here!
+```
+
+## Contribute
+
+PRs accepted.
+
+Small note: If editing the Readme, please conform to the [standard-readme](https://github.com/RichardLitt/standard-readme) specification.
+
+## License
+
+MIT © Patrick Harris
---
+++ solvers/anagram_solver.py
@@ -0,0 +1,62 @@
+#!/usr/local/bin/python3.5
+import itertools
+
+
+def find_possible(lst):
+    """
+    Return all possible combinations of letters in lst
+
+    @type lst: [str]
+    @rtype: [str]
+    """
+    returned_list = []
+
+    for i in range(0, len(lst) + 1):
+        for subset in itertools.permutations(lst, i):
+            possible = ''
+            for letter in subset:
+                possible += letter
+            if len(possible) == len(lst):
+                # itertools.permutations returns smaller lists
+                returned_list.append(possible)
+
+    return returned_list
+
+
+def return_words(lst, word_set):
+    """
+    Return combinations in that are words in word_set
+
+    @type lst: [str]
+    @type word_set: set(str)
+    @rtype: [str]
+    """
+    returned_list = []
+
+    for word in lst:
+        if word in word_set or word.capitalize() in word_set:
+            # Some words are capitalized in the word_set
+            returned_list.append(word)
+
+    return returned_list
+
+
+if __name__ == '__main__':
+    with open("words", "r", encoding='UTF-8') as words:
+        word_set = set(words.read().split())
+
+    anagram = input('Type an anagram: ')
+    anagram_lst = []
+    for char in anagram:
+        anagram_lst.append(char)
+
+    possible_words = find_possible(anagram_lst)
+    actual_words = return_words(possible_words, word_set)
+
+    print('Solutions:')
+    if len(actual_words) == 0:
+        print('None found')
+    else:
+        for item in set(actual_words):
+            # Running through in set form prevents duplicates
+            print(item)
